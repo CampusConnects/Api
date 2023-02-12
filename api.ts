2c1161e07fbedae5701be7559d79dbec06487763
LightLord <87213208+LightLordYT@users.noreply.github.com>
2023-02-11 19:35:25 -0800

    add logic to checking

--- api.ts
+++ api.ts
@@ -1,13 +1,12 @@
-import express from "npm:express@4.18.2";
-import { CookieDB } from "https://deno.land/x/cookie_driver@0.5.1/mod.ts";
+import express from "npm:express@4.18.2"
+import { CookieDB } from "https://deno.land/x/cookie_driver@0.5.1/mod.ts"
 
-const app = express();
+const app = express()
 // Initialize instance
 const cookieDB = new CookieDB(
-    "https://cookiedb.com/api/db",
-    "4RWuUXgRTYeEKDx6oufXdb3YdjqWfC15",
-  );
-
+	"https://cookiedb.com/api/db",
+	"4RWuUXgRTYeEKDx6oufXdb3YdjqWfC15"
+)
 
 app.use(express.json())
 
@@ -24,50 +23,63 @@ app.use(express.json())
 // });
 
 app.post("/api/", async (req, res) => {
-    const data = req.body
-    console.log(data)
-    if(data.userType == "admin"){
-        //cookiedb insert admin
-        await cookieDB.insert("admin", {
-          firstName: data.firstName,
-          lastName: data.lastName,
-          school: data.school,
-          email: data.email,
-          password: data.password,
-          age: data.age,
-        });
-    }else{
-        //cookiedb insert student
-        await cookieDB.insert("students", {
-            firstName: data.firstName,
-            lastName: data.lastName,
-            school: data.school,
-            email: data.email,
-            password: data.password,
-            grade: data.grade,
-        })
-    }
-    res.json("User succesfully added to database")
-});
-
-app.post('/api/confirm', async (req, res) => {
-    const data = req.body
-    console.log(data)
-    if(data.userType == "admin"){
-        //query database for admin
-        await cookieDB.select("admin", `eq($lastName, ${data.lastName})`)
-    }else{
-        //query database for student
-        await cookieDB.select("students", `eq($lastName, ${data.lastName})`)
-    }
+	const data = req.body
+	console.log(data)
+	if (data.userType == "admin") {
+		//cookiedb insert admin
+		await cookieDB.insert("admin", {
+			firstName: data.firstName,
+			lastName: data.lastName,
+			school: data.school,
+			email: data.email,
+			password: data.password,
+			age: data.age,
+		})
+	} else {
+		//cookiedb insert student
+		await cookieDB.insert("students", {
+			firstName: data.firstName,
+			lastName: data.lastName,
+			school: data.school,
+			email: data.email,
+			password: data.password,
+			grade: data.grade,
+		})
+	}
+	res.json("User succesfully added to database")
 })
 
-app.put('/api/updateUser', async (req, res) => {
-
+app.post("/api/confirm", async (req, res) => {
+	const data = req.body
+	console.log(data)
+	if (data.userType == "admin") {
+		//query database for admin
+		console.log("hi")
+		await cookieDB
+			.select("admin", `eq($email, '${data.email}')`)
+			.then((data) => {
+				if (data) {
+					//send back ok
+				} else {
+					//send back not ok
+				}
+			})
+	} else {
+		//query database for student
+		await cookieDB
+			.select("students", `eq($email, '${data.email}')`)
+			.then((data) => {
+				if (data) {
+					//send back ok
+				} else {
+					//send back not ok
+				}
+			})
+	}
 })
 
-app.delete('/api/deleteUser', async (req, res) => {
+app.put("/api/updateUser", async (req, res) => {})
 
-})
+app.delete("/api/deleteUser", async (req, res) => {})
 
-app.listen(8000, console.log("API Running on port 8000"));
+app.listen(8000, console.log("API Running on port 8000"))
